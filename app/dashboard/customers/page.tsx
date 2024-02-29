86dabb64c5f7973f1f3292b89e628e2034499d2a
Uta.li <uta.li@foxmail.com>
2024-02-29 09:32:02 +0800

    commit

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,3 @@
-export default () => {
+export default function Customers() {
   return <>Customer Page</>
 }
--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,6 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 
-export default ({ children }) => {
+export default function Layout ({ children }) {
   return <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
     <div className="w-full flex-none md:w-64">
       <SideNav />
